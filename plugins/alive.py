0acaa61840e86e359783db0013ff55dc6843b400
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-06-01 22:41:05 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -29,7 +29,7 @@ async def _human_time_duration(seconds):
     
    
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(command("tart") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/f7a3acc8e6e0e85f56150.jpg",
